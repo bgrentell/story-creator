51cd1613b86163d6813dfbdacf1743c75526708a
Brett Grentell <thereisnowithout@gmail.com>
2024-05-05 17:20:12 +1000

    Passage looping implemented

--- story.py
+++ story.py
@@ -9,8 +9,6 @@ except FileNotFoundError:
     print("Could not find the passages file.  Please make sure it exists!")
     exit(0)
     
-test_path = ["first", "second", "fourth", "end"] # Tests supposed options by the user.
-
 def serialize(path):
     fileText = ""
     for option in path:
@@ -28,7 +26,30 @@ def writeStory(fileText, filePath):
     except IOError:
         print("There was a problem writing the file.")        
         
+def main():
+    # Loops through story until an end result is reached.
+    current_passage = "begin" # The tag for the beginning passage
+    while current_passage != "end": # The tag for the end of story
+        current_passage = do_story(current_passage)
+    
+def do_story(current_passage):
+    # Gets the passage from the global passages list, displays the passage, then returns the next selected passage from the user.
+    passage = passages[0][current_passage]
+    print_passage(passage["passage"])
+    exit(0)
 
+def print_passage(passage):
+    # Prints the given passage
+    print(passage)
+    print()
+    
+##############
+# Test Suite #
+##############
+# test_path = ["first", "second", "fourth", "end"] # Tests supposed options by the user.
 
-text = serialize(test_path)
-writeStory(text, "teststory.txt")
+# text = serialize(test_path)
+# writeStory(text, "teststory.txt")
+
+if __name__ == '__main__':
+    main()
