839273f67b0e3f04e9e6cb899fb89651b5f7e144
Brett Grentell <thereisnowithout@gmail.com>
2024-05-05 16:11:09 +1000

    Added serialization feature

--- story.py
+++ story.py
@@ -2,7 +2,20 @@ import json
 
 passages = {}
 
-with open('passages.json') as p:
+with open('passages.json') as p: # Loads the passages file into dictonary object.
     passages = json.load(p)
     print(passages)
     
+test_path = ["first", "second", "fourth", "end"] # Tests supposed options by the user.
+
+def serialize(path):
+    fileText = ""
+    for option in path:
+        if(option == "end"):
+            break
+        
+        fileText += "\n\n" + passages[0][option]["passage"]
+    
+    print(fileText)
+
+serialize(test_path)
